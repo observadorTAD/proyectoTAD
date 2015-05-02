004bd6aec2ce88642d5f6e44940121639736c7ca
racede <racede@192.168.164.138>
2015-05-02 14:32:15 +0200

    RegistroFinalizado

--- src/main/java/controlador/UsuarioController.java
+++ src/main/java/controlador/UsuarioController.java
@@ -33,4 +33,5 @@ public class UsuarioController {
         return new String(md.digest(output.getBytes()));
     }
     
+    
 }
--- src/main/java/modelo/entidades/Artista.java
+++ src/main/java/modelo/entidades/Artista.java
@@ -1,27 +1,52 @@
 package modelo.entidades;
 
 /**
- * Esta clase contine los métodos y atributos que pertenecen a la entidad Artista
+ * Esta clase contine los métodos y atributos que pertenecen a la entidad
+ * Artista
  */
-public class Artista extends Persona {
+public class Artista {
 
-    private String nombreArtistico;
-    private String descripcion;
+    private final String email;
+    private final String password;
+    private final String nombre;
+    private final String apellidos;
+    private final String nombreArtistico;
+    private final String descripcion;
 
     /**
      * Constructor que sirve par crear un objeto de tipo Artista.
+     *
      * @param nombreArtistico
      * @param descripcion
      * @param email
      * @param password
      * @param nombre
-     * @param apellidos 
+     * @param apellidos
      */
     public Artista(String nombreArtistico, String descripcion, String email,
             String password, String nombre, String apellidos) {
-        super(email, password, nombre, apellidos);
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
         this.nombreArtistico = nombreArtistico;
         this.descripcion = descripcion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
     }
 
 }
